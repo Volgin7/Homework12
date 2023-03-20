5f7140ba01b196971b7a51488c40f5609f48317f
Volgin7 <8612020@gmail.com>
2023-03-20 16:10:32 +0300

    hw objects

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        Author firstAuthor = new Author("Stethen", "King");
+        Author secondAuthor = new Author("Leo", "Tolstoy");
+
+        Book firstBook = new Book("The Shining", firstAuthor, 1977);
+        System.out.println("'" + firstBook.getBookName() + "'" + " by " + firstBook.getBookAuthor().getFirstName() + " " + firstBook.getBookAuthor().getFamilyName()+ " was published in " + firstBook.getPublishingYear());
+        firstBook.setPublishingYear(2023);
+        System.out.println("'" + firstBook.getBookName() + "'" + " by " + firstBook.getBookAuthor().getFirstName() + " " + firstBook.getBookAuthor().getFamilyName()+ " was published in " + firstBook.getPublishingYear() + ", again");
+        System.out.println();
+
+        Book secondBook = new Book("Anna Karenina", secondAuthor, 1877);
+        System.out.println("'" + secondBook.getBookName() + "'" + " by " + secondBook.getBookAuthor().getFirstName() + " " + secondBook.getBookAuthor().getFamilyName()+ " was published in " + secondBook.getPublishingYear());
+        secondBook.setPublishingYear(2002);
+        System.out.println("'" + secondBook.getBookName() + "'" + " by " + secondBook.getBookAuthor().getFirstName() + " " + secondBook.getBookAuthor().getFamilyName()+ " was published in " + secondBook.getPublishingYear() + ", again");
+
+
+        // The Shining
+    } // end of main
+} //end of Main
